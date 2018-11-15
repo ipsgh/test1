4ff6ada75db6fc34fd3c374102b05f8da566a1b6
isaacjava <isaacps73@gmail.com>
2018-11-15 18:53:48 +0100

    commit 3

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hi, mate");
 		System.out.println("Hi, pal 22222");
-
+		System.out.println("Hi, budy 333333");
 	}
 
 }
